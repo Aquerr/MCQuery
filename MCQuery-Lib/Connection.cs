7cde3ca32d56e414fec80b97981e999978aaec0e
Bartłomiej Stępień <nerdianin@gmail.com>
2018-01-23 16:20:52 +0100

    Started working on TCP Client

---
+++ MCQuery-Lib/Connection.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+
+namespace MCQuery_Lib
+{
+    public class Connection
+    {
+        public Connection(string address, string port)
+        {
+            TcpClient client = new TcpClient(address, int.Parse(port));
+
+            try
+            {
+             //   client.Connect();
+            }
+            catch (SocketException socketException)
+            {
+                //TODO:Do something with exception...
+            }
+        }
+    }
+}
---
+++ MCQuery-Lib/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCQuery_Lib
+{
+    public class Server
+    {
+        public string Name { get; set; }
+        public List<String> PlayerList { get; set; }
+        public string Motd { get; set; }
+        public string GameMode { get; set; }
+        public bool Status { get; set; }
+
+        public Server(string name, bool status)
+        {
+            if (status)
+            {
+
+            }
+        }
+
+        public Server(string name, string gamemode, bool status, List<String> playerlist)
+        {
+
+        }
+    }
+}
